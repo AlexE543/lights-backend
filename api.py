799f81677203a851b51b3e654ae37a3c94ec8619
Alex Ellison <alexe@DESKTOP-0U8EAR3.mit.edu>
2020-12-26 11:40:16 -0600

    test endpoints

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restx import Api, Resource
+from light_strand import LightStrand
 import time
 import board
 import neopixel
@@ -8,11 +9,32 @@ app = Flask(__name__)
 api = Api(app)
 
 
-@app.route('/hello')
+@api.route('/hello')
 class Hello(Resource):
     def get(self):
         return "Hello!"
 
 
+@api.route('/clear')
+class Clear(Resource):
+    def get(self):
+        start = time.time()
+        light_strand.clear()
+        end = time.time()
+        return f"This command took {end-start} seconds"
+
+@api.route('/fill')
+@api.doc(params={
+    'r': "red color out of 255",
+    'g': "green color out of 255",
+    'b': "blue color out of 255",
+})
+class Fill(Resource):
+    def post(self):
+        r, g, b = request.args.get('r'), request.args.get('g'), request.args.get('b')
+        return f"({r}, {g}, {b})"
+
+
 if __name__ == "__main__":
+    light_strand = LightStrand(144, .2)
     app.run()
