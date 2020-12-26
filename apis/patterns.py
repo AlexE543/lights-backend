e942b76a4c0ad58dbcb74c781d7af839ed3ec2b8
Alex Ellison <alexe@DESKTOP-0U8EAR3.mit.edu>
2020-12-26 13:33:44 -0600

    testing

--- apis/patterns.py
+++ apis/patterns.py
@@ -23,8 +23,6 @@ class Hello(Resource):
         color_two = request.form.get('color_two')
         print(color_one)
         print(type(color_one))
-        print(tuple(color_one))
-        print(type(tuple(color_one)[0]))
         return "Hello!"
 
 
