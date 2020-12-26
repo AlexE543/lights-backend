57f5adcbe83fd24a72ab1030bd046ae4ea1c9890
Alex Ellison <alexe@DESKTOP-0U8EAR3.mit.edu>
2020-12-26 15:02:31 -0600

    changed formatting

--- apis/basics.py
+++ apis/basics.py
@@ -2,6 +2,7 @@ from flask import request
 from flask_restx import Resource, Namespace
 from utils.shared import light_strand
 import time
+import json
 
 basics_ns = Namespace("basics", description="basic led control")
 
@@ -23,49 +24,47 @@ class Clear(Resource):
 
 @basics_ns.route('/fill')
 @basics_ns.doc(params={
-    'r': "red color out of 255",
-    'g': "green color out of 255",
-    'b': "blue color out of 255",
+    'color': "list of [r, g, b]"
 })
 class Fill(Resource):
     def post(self):
         start = time.time()
-        r, g, b = int(request.args.get('r')), int(request.args.get('g')), int(request.args.get('b'))
-        light_strand.fill((r, g, b))
+        data = json.loads(request.data)
+        color = tuple(data.get('color'))
+        light_strand.fill(color)
         end = time.time()
-        return f"Filling with ({r}, {g}, {b}) took {end-start} seconds"
+        return f"Filling with {color} took {end-start} seconds"
 
 
 @basics_ns.route('/fill_range')
 @basics_ns.doc(params={
-    'r': "red color out of 255",
-    'g': "green color out of 255",
-    'b': "blue color out of 255",
+    'color': "list of [r, g, b]",
     'start': "start index",
     'end': "end index"
 })
 class FillRange(Resource):
     def post(self):
         start = time.time()
-        r, g, b = int(request.args.get('r')), int(request.args.get('g')), int(request.args.get('b'))
+        data = json.loads(request.data)
+        color = tuple(data.get('color'))
         start_index, end_index = int(request.args.get('start')), int(request.args.get('end'))
-        light_strand.fill_range(start_index, end_index, (r, g, b))
+        light_strand.fill_range(start_index, end_index, color)
         end = time.time()
         return f"Filling the range took {end-start} seconds"
 
+
 @basics_ns.route('/set_pixel')
 @basics_ns.doc(params={
-    'r': "red color out of 255",
-    'g': "green color out of 255",
-    'b': "blue color out of 255",
+    'color': "list of [r, g, b]",
     'index': "start index",
 })
 class SetPixel(Resource):
     def post(self):
         start = time.time()
-        r, g, b = int(request.args.get('r')), int(request.args.get('g')), int(request.args.get('b'))
+        data = json.loads(request.data)
+        color = tuple(data.get('color'))
         idx = int(request.args.get('index'))
-        light_strand.set_pixel(idx, (r, g, b))
+        light_strand.set_pixel(idx, color)
         end = time.time()
         return f"Setting the pixel took {end-start} seconds"
 
