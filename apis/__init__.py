d9676ff93ec9148bb76a665616ca5e9e480fd27b
Alex Ellison <alexe@DESKTOP-0U8EAR3.mit.edu>
2020-12-27 12:22:28 -0600

    added flask cors

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restx import Api
+from flask_cors import CORS
 from .basics import basics_ns
 from .patterns import patterns_ns
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     api = Api(
         title="lights-backend",
         version='0.1.0',
