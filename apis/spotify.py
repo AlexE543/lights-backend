8bac6b0f4c34ae7216637e59c39540c97efa2d50
Alex Ellison <alexe@DESKTOP-0U8EAR3.mit.edu>
2020-12-28 15:37:09 -0600

    basic apis for spotify

--- apis/spotify.py
+++ apis/spotify.py
@@ -16,11 +16,42 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=os.getenv(
                                                 client_secret=os.getenv('SPOTIPY_CLIENT_SECRET')))
 
 
-@spotify_ns.route('/clear')
-class GetUser(Resource):
+@spotify_ns.route('/test')
+class Test(Resource):
     def get(self):
         start = time.time()
         print(sp.current_playback())
         print(sp.currently_playing())
+        end = time.time()
+        return f"This command took {end-start} seconds"
+
+
+@spotify_ns.route('/next_song')
+class NextSong(Resource):
+    def get(self):
+        start = time.time()
+        print(sp.next_track())
+        end = time.time()
+        return f"This command took {end-start} seconds"
+
+
+@spotify_ns.route('/previous_song')
+class PreviousSong(Resource):
+    def get(self):
+        start = time.time()
+        print(sp.previous_track())
+        end = time.time()
+        return f"This command took {end-start} seconds"
+
+
+@spotify_ns.route('/pause_play')
+class PausePlay(Resource):
+    def get(self):
+        start = time.time()
+        print(sp.currently_playing())
+        if sp.currently_playing():
+            print(sp.pause_playback())
+        else:
+            print(sp.start_playback())
         end = time.time()
         return f"This command took {end-start} seconds"
