d00891cfc2d745cc4912e29b188ee29fdb242886
Alex Ellison <alexe@DESKTOP-0U8EAR3.mit.edu>
2020-12-28 14:19:01 -0600

    auth fixes

--- utils/spotify_authorization.py
+++ utils/spotify_authorization.py
@@ -1,9 +1,11 @@
 import spotipy
+import os
 from spotipy.oauth2 import SpotifyOAuth
 
 scope = "user-library-read"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=os.getenv('SPOTIPY_CLIENT_ID'),
+                                               client_secret=os.getenv('SPOTIPY_CLIENT_SECRET')))
 results = sp.current_user_saved_tracks()
 for idx, item in enumerate(results['items']):
     track = item['track']
